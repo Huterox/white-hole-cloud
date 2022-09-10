004e508dd9991daee98a7db43cbfddb9b2b6b1e9
Huterox <3139541502@qq.com>
2022-09-10 17:49:36 +0800

    v0.14完成人人后端整合

--- whitehole-community/src/main/java/com/huterox/whitehole/community/WhiteHoleCommunityApplication.java
+++ whitehole-community/src/main/java/com/huterox/whitehole/community/WhiteHoleCommunityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityApplication {
+public class WhiteHoleCommunityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommunityApplication.class, args);
+        SpringApplication.run(WhiteHoleCommunityApplication.class, args);
     }
 
 }
