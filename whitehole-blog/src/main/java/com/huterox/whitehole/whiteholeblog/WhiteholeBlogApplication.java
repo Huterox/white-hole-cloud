a7a8822af42ed231996f3e884e367cf315a02fb7
Huterox <3139541502@qq.com>
2022-09-10 09:51:48 +0800

    完成基本的项目构建

---
+++ whitehole-blog/src/main/java/com/huterox/whitehole/whiteholeblog/WhiteholeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.huterox.whitehole.whiteholeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteholeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteholeBlogApplication.class, args);
+    }
+
+}
---
+++ whitehole-blog/src/test/java/com/huterox/whitehole/whiteholeblog/WhiteholeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huterox.whitehole.whiteholeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteholeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
