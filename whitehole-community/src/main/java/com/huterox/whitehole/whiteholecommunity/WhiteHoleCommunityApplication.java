d9ee70157c898f122ee2ae8eec7350ee52d234ec
Huterox <3139541502@qq.com>
2022-09-17 22:55:27 +0800

    v0.21整合nacos

--- whitehole-community/src/main/java/com/huterox/whitehole/whiteholecommunity/WhiteHoleCommunityApplication.java
+++ whitehole-community/src/main/java/com/huterox/whitehole/whiteholecommunity/WhiteHoleCommunityApplication.java
@@ -1,9 +1,13 @@
 package com.huterox.whitehole.whiteholecommunity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.huterox.whitehole.whiteholecommunity.dao")
 public class WhiteHoleCommunityApplication {
 
     public static void main(String[] args) {
